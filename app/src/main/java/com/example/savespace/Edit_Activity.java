7070c4cc778d548fc1214d95f0a981f4658ded6e
Ch35tEr <48945081+Ch35tEr@users.noreply.github.com>
2020-07-19 01:22:34 +0200

    Simple Complete
    Simple design of main activity and edit activity complete

--- app/src/main/java/com/example/savespace/Edit_Activity.java
+++ app/src/main/java/com/example/savespace/Edit_Activity.java
@@ -3,12 +3,36 @@ package com.example.savespace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Edit_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO:
+        //      Retrieve data from clicked note
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
     }
+
+    public void doBack(View v) {
+        finish();
+    }
+
+    public void doSave(View v) {
+        // TODO :
+        //      Save note into the Database and update last saved
+        EditText note = findViewById(R.id.text_note);
+        String message = note.getText().toString();
+        Toast.makeText(this, "Note : "+message, Toast.LENGTH_LONG).show();
+    }
+
+    /*
+    TODO:
+        Link notes to database
+
+     */
 }
--- app/src/main/java/com/example/savespace/MainActivity.java
+++ app/src/main/java/com/example/savespace/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.savespace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,7 +14,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<SpaceNote> spaceNotes;
-    ArrayList<String> food;
     //SpaceAdapter spaceAdapter;
 
     @Override
@@ -19,56 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        food = new ArrayList<String>();
-        food.add("abx");
-        food.add("gang");
-        food.add("last words");
-        food.add("incels");
-        food.add("ISIS");
-        food.add("Karen");
-        food.add("Terrorists");
-        food.add("mixer");
-        food.add("Objective");
-        food.add("Overdose");
-        food.add("positive");
-        food.add("Trying");
-        food.add("Murder");
-
         SpaceNote a = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote b = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote c = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote d = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote e = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote f = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote g = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote h = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote i = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote j = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote k = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-        SpaceNote l = new SpaceNote(0, "Hello World", "Werukamu tsu za warudo!!!", "16-02-2020", "00:00");
-
 
         spaceNotes = new ArrayList<>();
         spaceNotes.add(a);
-        spaceNotes.add(b);
-        spaceNotes.add(c);
-        spaceNotes.add(d);
-        spaceNotes.add(e);
-        spaceNotes.add(f);
-        spaceNotes.add(g);
-        spaceNotes.add(h);
-        spaceNotes.add(i);
-        spaceNotes.add(j);
-        spaceNotes.add(k);
-        spaceNotes.add(l);
-
-        //spaceAdapter = new SpaceAdapter(spaceNotes, this, R.layout.note_list);
-        //ListView main_list = findViewById(R.id.main_list);
-        //main_list.setAdapter(spaceAdapter);
 
         ListView main_list = findViewById(R.id.main_list);
         SpaceAdapter spaceAdapter = new SpaceAdapter(spaceNotes, this);
         main_list.setAdapter(spaceAdapter);
+
+        main_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent edit = new Intent(getApplicationContext(), Edit_Activity.class);
+                startActivity(edit);
+            }
+        });
 
     }
 
